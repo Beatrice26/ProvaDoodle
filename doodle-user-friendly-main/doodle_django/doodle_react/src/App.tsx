a3a3fd5d6d60b4e94d7c3f9255e4b5518754f78b
Beatrice Motta <mottabea@gmail.com>
2023-11-11 17:14:12 +0100

    first

---
+++ doodle-user-friendly-main/doodle_django/doodle_react/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {CalendarBaseComponent} from './UI/Calendar/Components/CalendarBase';
+import {FormComponent} from './UI/Calendar/Components/Form';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormComponent />
+        
+    </div>
+  );
+}
+
+export default App;
